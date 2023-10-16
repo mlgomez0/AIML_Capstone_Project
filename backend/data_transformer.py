65f12bf3b2c1e2dcfaf8a10e545e6764cc3b4d0b
Bhavya Vadher <89723648+BhavyaVadher@users.noreply.github.com>
2023-10-16 16:56:05 -0400

    Update data_transformer.py
    add some methods to make tokens

--- backend/data_transformer.py
+++ backend/data_transformer.py
@@ -1,10 +1,20 @@
+import spacy
 from langchain.embeddings.spacy_embeddings import SpacyEmbeddings
 from langchain.embeddings import HuggingFaceEmbeddings
 
 class DataTransformer:
     def __init__(self):
-        pass
+        self.nlp = spacy.load("en_core_web_sm")
 
+    def clean_text_with_nlp(self, text):
+        doc = self.nlp(text)
+        cleaned_tokens = [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
+        cleaned_text = ' '.join(cleaned_tokens)
+        return cleaned_text
+
+    def tokenize_words(self, text):
+        tokens = text.split()
+        return tokens
 
     def get_spacy_embedding(self, tokens):
         embedder = SpacyEmbeddings()

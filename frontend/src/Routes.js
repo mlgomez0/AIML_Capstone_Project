f7d6231a2be6bd123ea71bc8747d8ac639a7bd18
Mary Gomez <marygomez@marys-mbp.home>
2023-11-29 11:35:50 -0500

    feat(merge): with main branch

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom'
 import App from './App'
 import  Welcome from './Welcome'
+import Header from "./header";
+import Footer from "./footer";
 export const Routes = () => {
     return (
         <Router>
+            <Header />
             <Switch>
                 <Route path="/" element={<Welcome />} />
                 <Route path="/home" element={<App />} />
             </Switch>
-
+            <Footer />
         </Router>
     )
 
